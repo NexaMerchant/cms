6725a9f932b210777fb45b68ede458ae3e13abe6
Steve <34465153+xxl4@users.noreply.github.com>
2024-10-29 10:08:35 +0800

    fix update db

--- src/Database/Migrations/2024_08_20_185850_create_cms_tables.php
+++ src/Database/Migrations/2024_08_20_185850_create_cms_tables.php
@@ -35,36 +35,83 @@ return new class extends Migration
         // create cms categories table
         Schema::create('cms_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->integer('parent_id')->default(0);
+            $table->integer('order')->default(0);
+            $table->integer('status')->default(1);
+            $table->integer('created_by')->default(0);
+            $table->integer('updated_by')->default(0);
+            $table->integer('deleted_by')->default(0);
+            $table->softDeletes();
+            $table->integer('deleted_at')->nullable();
             $table->timestamps();
         });
 
         // create cms categories translations table
         Schema::create('cms_categories_translations', function (Blueprint $table) {
             $table->id();
+            $table->integer('cms_category_id');
+            $table->string('locale');
+            $table->string('name');
+            $table->string('slug');
             $table->timestamps();
         });
 
         // create cms tags table
         Schema::create('cms_tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->integer('status')->default(1);
+            $table->integer('created_by')->default(0);
+            $table->integer('updated_by')->default(0);
+            $table->integer('deleted_by')->default(0);
+            $table->softDeletes();
             $table->timestamps();
         });
 
         // create cms tags translations table
         Schema::create('cms_tags_translations', function (Blueprint $table) {
             $table->id();
+            $table->integer('cms_tag_id');
+            $table->string('locale');
+            $table->string('name');
+            $table->string('slug');
             $table->timestamps();
         });
 
         // create cms posts table
         Schema::create('cms_posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->integer('status')->default(1);
+            $table->integer('created_by')->default(0);
+            $table->integer('updated_by')->default(0);
+            $table->integer('deleted_by')->default(0);
+            $table->softDeletes();
+            $table->integer('deleted_at')->nullable();
+            $table->integer('category_id');
+            $table->integer('featured')->default(0);
+            $table->integer('views')->default(0);
+            $table->integer('likes')->default(0);
+            $table->integer('dislikes')->default(0);
+            $table->integer('shares')->default(0);
+            $table->integer('comments')->default(0);
+            $table->integer('published_at')->nullable();
             $table->timestamps();
         });
 
         // create cms posts translations table
         Schema::create('cms_posts_translations', function (Blueprint $table) {
             $table->id();
+            $table->integer('cms_post_id');
+            $table->string('locale');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -75,5 +122,13 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('cms');
+        Schema::dropIfExists('cms_post_meta');
+        Schema::dropIfExists('cms_categories');
+        Schema::dropIfExists('cms_categories_translations');
+        Schema::dropIfExists('cms_tags');
+        Schema::dropIfExists('cms_tags_translations');
+        Schema::dropIfExists('cms_posts');
+        Schema::dropIfExists('cms_posts_translations');
+
     }
 };
